5c5d92569ebf2b2eec5197922186348ccea5eafb
Mo2men7 <momen.helmy.m@gmail.com>
2024-07-03 18:32:55 +0300

    Handle adding a new item to the list (The Concept of Lifting State Up and Understanding the child to parent communication

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState(initialItems);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -19,7 +23,7 @@ export default function App() {
 function Logo() {
   return <h1>🧳 Far Away 🛫</h1>;
 }
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -27,8 +31,7 @@ function Form() {
     e.preventDefault();
     if (!description) return;
     const newItem = { id: Date.now(), description, quantity, packed: false };
-    const newItemsArray = [...initialItems, newItem];
-    console.log(newItemsArray);
+    onAddItems(newItem);
     setDescription("");
     setQuantity(1);
   }
@@ -58,11 +61,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
