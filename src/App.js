ab08bb9539318b3514953359519e5c1012931e9d
Mo2men7 <momen.helmy.m@gmail.com>
2024-07-03 19:46:56 +0300

    Calculating Statistics as Derived State and Use Them in Footer

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ export default function App() {
         onToggleItem={handleTogglItem}
         onDeleteItem={handleDeleteItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -108,10 +108,26 @@ function Item({ item, deleteItem, toggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  // Calculating Statistics as Derived State
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start add some items to your packing list💼🧳</em>
+      </p>
+    );
+  }
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
-      <em>You have x items on ur list, U already packed x (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything, Ready to GOOO 🚀"
+          : `You have ${numItems} items on ur list, U already packed ${numPacked} (
+          ${percentage}%)`}
+      </em>
     </footer>
   );
 }
